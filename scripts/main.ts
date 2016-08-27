7d0483ad87523f74aa933d3203f0813409e98239
Mark Yep <ooksei@outlook.com>
2016-08-27 16:46:19 +1200

    eventListeners for radiobuttons and button

--- scripts/main.ts
+++ scripts/main.ts
@@ -4,8 +4,24 @@ var conversionSelected : string = "setLocal";
 function init() {
     document.getElementById("currencyForm").addEventListener("click", function(){
         currencySelected = document.querySelector('input[name=currency]:checked').value;
+        document.getElementById("statusText").innerHTML = "You have selected " + currencySelected + ".";
+        setPlaceholderText();
     });
     document.getElementById("conversionForm").addEventListener("click", function(){
-         conversionSelected = document.querySelector('input[name=conversion]:checked').value;
+         setPlaceholderText();
     });
+    document.getElementById("goButton").addEventListener("click", function(){
+         alert("You want to " + conversionSelected + " with " + currencySelected);
+    });
+}
+
+function setPlaceholderText () {
+    conversionSelected = document.querySelector('input[name=conversion]:checked').value;
+    if (conversionSelected === "setLocal") {
+        document.getElementById("inputBox").placeholder = "Enter how much NZD you want to convert to " + currencySelected;
+    } else if (conversionSelected === "setForeign"){
+        document.getElementById("inputBox").placeholder = "Enter how much " + currencySelected + " you want to buy";
+    } else {
+        document.getElementById("inputBox").placeholder = "Enter how much " + currencySelected + " you want to sell";
+    }
 }
